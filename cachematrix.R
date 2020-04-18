ff3ec62b9c1d2015da9e4ad7f12a28b2a3562281
reemkurdi <reemkurdi@gmail.com>
2020-04-18 13:40:45 +0400

    Update cachematrix.R
    Add some comments to test whether github desktop and github browser are integrated

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,17 @@
 
 ## Write a short comment describing this function
 
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
-
+## cacheSolve computes inverse of the special "matrix" returned by makeCacheMatrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## Checks whether the inverse has already been calculated or not 
+        ## If it has, and matrix has not changed then cacheSolve retrieves
+        ## inverse from the cache
 }
